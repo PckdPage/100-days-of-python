190547a9169c483036915ada1882d9169e40cd7e
PckdPage <samyekgalaxy146@outlook.com>
2025-06-19 13:45:15 +0545

    Day 01: Learned basic print and variables

---
+++ Day 1/day 1.py
@@ -0,0 +1,22 @@
+
+print("Hello, Python World!")
+
+name = "Samyek"
+print(f"Hello, {name}! Welcome to Python Programming")
+
+userName = input("What is your name? ")
+print(f"Hello, {userName}! Welcome to Python Programming")
+
+# Welcome Message Generator
+
+# Step 1: Ask for user details
+name = input("What's your name? ")
+hobby = input("What's your favorite hobby? ")
+
+# Step 2: Generate a personalized welcome message
+print("\n--- Welcome Message ---")
+print(f"Hello, {name}! ")
+print("Welcome to the world of Python Programming.")
+print(f"It's great to know that you love {hobby}.")
+print("Get ready to build something amazing today.")
+
